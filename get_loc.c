fe90d985e2cc8d09484217e0ad847123d8ba8ddb
kaleabendrias <kaleabendrias1212@gmail.com>
2023-05-13 13:56:20 +0300

    fix

--- get_loc.c
+++ get_loc.c
@@ -8,38 +8,45 @@
 
 char *get_loc(char *command)
 {
-    char *path, *path_copy, *path_token, *file_path;
-    int command_len, directory_len;
-    struct stat buffer;
+	char *path, *path_copy, *path_token, *file_path;
+	int command_len, directory_len;
+	struct stat buffer;
 
-    path = getenv("PATH");
-    if (path)
-    {
-        path_copy = _strdup(path);
-        command_len = _strlen(path);
-        path_token = _strtok(path_copy, ":");
-        while (path_token != NULL)
-        {
-            directory_len = _strlen(path_token);
-            file_path = malloc(command_len + directory_len + 2);
-            _strcpy(file_path, path_token);
-            _strcat(file_path, "/");
-            _strcat(file_path, command);
-            _strcat(file_path, "\0");
-            if (stat(file_path, &buffer) == 0)
-            {
-                free(path_copy);
-                return file_path;
-            }
-            free(file_path);
-            path_token = _strtok(NULL, ":");
-        }
-        if (stat(command, &buffer) == 0)
-        {
-            free(path_copy);
-            return command;
-        }
-        free(path_copy);
-    }
-    return NULL;
+	if (command == NULL || *command == '\0')
+        return NULL;
+	path = getenv("PATH");
+	if (path)
+	{
+		path_copy = _strdup(path);
+		command_len = _strlen(command);
+		path_token = _strtok(path_copy, ":");
+		while (path_token != NULL)
+		{
+			directory_len = _strlen(path_token);
+			file_path = malloc(command_len + directory_len + 2);
+			_strcpy(file_path, path_token);
+			_strcat(file_path, "/");
+			_strcat(file_path, command);
+			_strcat(file_path, "\0");
+			if (stat(file_path, &buffer) == 0)
+			{
+				free(path_copy);
+				return (file_path);
+			}
+			else
+			{
+				free(file_path);
+				path_token = _strtok(NULL, ":");
+			}
+		}
+		if (stat(command, &buffer) == 0)
+		{
+			free(path_copy);
+			return (command);
+
+		}
+		free(path_copy);
+	}
+	return (NULL);
 }
+
