0ac0489651c6a227eb256da24c48f7d8fabab27f
Yalelet Dessalegn <125442134+Yab8702@users.noreply.github.com>
2023-05-23 09:28:50 +0100

    Update

--- small_func2.c
+++ small_func2.c
@@ -34,3 +34,48 @@ void _unsetenv(char **args)
 		write(STDERR_FILENO, "Failed to unset environment variable\n", 37);
 	}
 }
+
+/**
+ * cd_command - handles the cd command
+ * @args: args
+ * Return: none
+ */
+void cd_command(char **args)
+{
+	char *dir = args[1];
+	char cwd[PATH_MAX], new_cwd[PATH_MAX];
+
+	if (dir == NULL || _strcmp(dir, "~") == 0)
+	{
+		dir = getenv("HOME");
+	}
+	else if (_strcmp(dir, "-") == 0)
+	{
+		dir = getenv("OLDPWD");
+	}
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	{
+		write(STDERR_FILENO, "Failed to get current working directory\n", 40);
+		return;
+	}
+	if (chdir(dir) != 0)
+	{
+		write(STDERR_FILENO, "Failed to change directory\n", 27);
+		return;
+	}
+	if (setenv("OLDPWD", cwd, 1) != 0)
+	{
+		write(STDERR_FILENO, "Failed to set OLDPWD environment variable\n", 42);
+		return;
+	}
+	if (getcwd(new_cwd, sizeof(new_cwd)) == NULL)
+	{
+		write(STDERR_FILENO, "Failed to get current working directory\n", 40);
+		return;
+	}
+	if (setenv("PWD", new_cwd, 1) != 0)
+	{
+		write(STDERR_FILENO, "Failed to set PWD environment variable\n", 39);
+		return;
+	}
+}
