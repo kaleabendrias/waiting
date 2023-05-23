f3d969322feee8d79b5020df7081f4aaa8b6b5b2
Yalelet Dessalegn <125442134+Yab8702@users.noreply.github.com>
2023-05-23 09:28:17 +0100

    Update

--- small_func2.c
+++ small_func2.c
@@ -18,3 +18,19 @@ void _setenv(char **args)
 		write(STDERR_FILENO, "Failed to set environment variable\n", 35);
 	}
 }
+/**
+ * _unsetenv - handles the unsetenv command
+ * @args: args
+ * Return: none
+ */
+void _unsetenv(char **args)
+{
+	if (args[1] == NULL)
+	{
+		write(STDERR_FILENO, "Usage: unsetenv VARIABLE\n", 25);
+	}
+	else if (unsetenv(args[1]) != 0)
+	{
+		write(STDERR_FILENO, "Failed to unset environment variable\n", 37);
+	}
+}
